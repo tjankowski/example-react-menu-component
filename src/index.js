c544f55cedc8cab554ef18ec51269cb4f87b5b03
Tomasz Jankowski <tomasz.jankowski@idea-inspired.com>
2020-04-19 21:00:09 +0200

    Render new application component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.scss';
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -15,3 +15,10 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+  console.log(
+      '\n' +
+      "Hello, nice to see you.\n\n" +
+      ' ( ͡° ͜ʖ ͡°)\n\n'
+  );
