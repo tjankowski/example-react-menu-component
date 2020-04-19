84a3eb32b69d3ff7868cd03154cc2698b127a390
Tomasz Jankowski <tomasz.jankowski@idea-inspired.com>
2020-04-19 20:45:19 +0200

    Add test data from API
    Test data will be used during API tests

---
+++ src/test/data.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    id: 0,
+    name: "Item0",
+    subItems: [
+      {
+        id: 0,
+        name: "SubMenu0",
+        subItems: [
+          {
+            id: 0,
+            name: "SubMenu0",
+          },
+          {
+            id: 1,
+            name: "SubMenu1",
+          },
+          {
+            id: 2,
+            name: "SubMenu2",
+          },
+          {
+            id: 3,
+            name: "SubMenu3",
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "SubMenu1",
+      },
+      {
+        id: 2,
+        name: "SubMenu2",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Item1",
+    subItems: [
+      {
+        id: 0,
+        name: "SubMenu0",
+      },
+      {
+        id: 1,
+        name: "SubMenu1",
+        subItems: [
+          {
+            id: 0,
+            name: "SubMenu0",
+          },
+          {
+            id: 1,
+            name: "SubMenu1",
+          },
+          {
+            id: 2,
+            name: "SubMenu2",
+          },
+          {
+            id: 3,
+            name: "SubMenu3",
+          },
+        ],
+      },
+      {
+        id: 2,
+        name: "SubMenu2",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Item2",
+    subItems: [
+      {
+        id: 0,
+        name: "SubMenu0",
+      },
+      {
+        id: 1,
+        name: "SubMenu1",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Item3",
+  },
+];
