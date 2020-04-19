06f15c71303e8ff9e8a8503b0b20105a0e6762eb
Tomasz Jankowski <tomasz.jankowski@idea-inspired.com>
2020-04-19 20:46:34 +0200

    Fetch data from API

---
+++ src/api/index.js
@@ -0,0 +1,7 @@
+const URL = "http://www.mocky.io/v2/5d3fec2b33000062009d27bc";
+
+export async function fetchData() {
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
